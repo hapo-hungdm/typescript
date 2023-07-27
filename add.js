df0eddbe353e60ea7c60ddaf5ffa49b26ac3ce8f
manh hung <hungdm@haposoft.com>
2023-07-27 09:57:59 +0700

    code main 2

--- add.js
+++
@@ -1,5 +0,0 @@
-var add = function (a, b) {
-    console.log(11111111111111);
-    return a + b;
-};
-add(2, 3);
--- add.ts
+++ add.ts
@@ -3,3 +3,10 @@ let add = (a:number, b:number):number =>{
     return a + b
 }
 add(2,3)
+
+// todo main new code
+var sub2 = function (a, b) {
+    console.log(2222222222222222);
+    return a + b;
+};
+add(2, 3);
